bb8e2b0a10fea68adb4877ab7a861393d641aeba
Clara Smith <claraeksmith@gmail.com>
2026-06-05 09:52:55 -0400

    change node/edge size

--- js/main.js
+++ js/main.js
@@ -67,10 +67,10 @@ function initSigma(config) {
     	graphProps=config.sigma.graphProperties;
     else
     	graphProps={
-        minNodeSize: 1,
-        maxNodeSize: 7,
-        minEdgeSize: 0.2,
-        maxEdgeSize: 0.5
+        minNodeSize: 10,
+        maxNodeSize: 20,
+        minEdgeSize: 3,
+        maxEdgeSize: 5
     	};
 	
 	if (config.sigma && config.sigma.mouseProperties) 
